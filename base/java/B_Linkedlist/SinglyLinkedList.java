e0792e3a9ca750da43c2392a1e06849dc80948d1
wangning <1151089590@qq.com>
2021-01-27 17:38:17 +0800

    提交代码

--- base/java/B_Linkedlist/SinglyLinkedList.java
+++ base/java/B_Linkedlist/SinglyLinkedList.java
@@ -2,5 +2,29 @@ package B_Linkedlist;
 
 public class SinglyLinkedList {
 
+    private Node head = null;
 
+    public Node findByValue(int value) {
+
+        Node p = head;
+        while (p != null && p.getData() != value) {
+            p = p.next;
+        }
+        return p;
+    }
+
+
+    public class Node {
+        private int data;
+        private Node next;
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+
+        public int getData() {
+            return this.data;
+        }
+    }
 }
