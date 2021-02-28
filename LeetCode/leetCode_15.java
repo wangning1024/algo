e672f64021767213cd35d91e0504b71449302cd3
wangning <1151089590@qq.com>
2021-02-28 21:02:40 +0800

    提交代码

---
+++ LeetCode/leetCode_15.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  三数之和
+ */
+public class leetCode_15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3) {
+            return new ArrayList<>();
+        }
+
+
+        return null;
+    }
+}
