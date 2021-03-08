4738ad459afd6417369595638ee45973c1f7557a
wangning <1151089590@qq.com>
2021-03-08 22:08:03 +0800

    提交代码

--- LeetCode/lc_17_电话号码字母组合/Solution.java
+++ LeetCode/lc_17_电话号码字母组合/Solution.java
@@ -23,18 +23,50 @@ public class Solution {
         numAbc.put("7", l7);
         numAbc.put("8", l8);
         numAbc.put("9", l9);
+        List<String> re = new ArrayList<>();
 
-        char[] chars = digits.toCharArray();
-        for (char c : chars) {
-            String[] strings = numAbc.get(c);
-            for (String str : strings) {
-
-            }
+        if (digits == null || digits.length() == 0) {
+            return re;
         }
 
+        solution(digits, 0, new StringBuilder(), re, numAbc);
 
+//        String[] strings = numAbc.get(String.valueOf(chars[0]));
+//        List<String> re = new ArrayList<>();
+//        for (String str : strings) {
+//            String[] strings1 = numAbc.get(String.valueOf(chars[1]));
+//            for (String str1 : strings1) {
+//                String[] strings2 = numAbc.get(String.valueOf(chars[2]));
+//                for (String str2 : strings2) {
+//                    re.add(str + str1 + str2);
+//                }
+//            }
+//        }
+        for (String s : re) {
+            System.out.println(s);
+        }
         return null;
 
     }
+
+    private void solution(String digits, int index, StringBuilder str, List<String> r, Map<String, String[]> numAbc) {
+        if (index >= digits.length()) {
+            r.add(str.toString());
+            return;
+        }
+        String[] strings = numAbc.get(digits.substring(index, index + 1));
+        for (int i = 0; i < strings.length; i++) {
+            str.append(strings[i]);
+            solution(digits, index + 1, str, r, numAbc);
+            // 递归结束后，需要把值充值会递归前的模样
+            str.deleteCharAt(str.length() - 1);
+        }
+    }
+
+    public static void main(String[] args){
+        Solution solution = new Solution();
+        solution.letterCombinations("57893");
+    }
+
 
 }
---
+++ LeetCode/lc_22_括号生成/Solution.java
@@ -0,0 +1,11 @@
+package lc_22_括号生成;
+
+import java.util.List;
+
+public class Solution {
+
+    public List<String> generateParenthesis(int n) {
+
+        return null;
+    }
+}
